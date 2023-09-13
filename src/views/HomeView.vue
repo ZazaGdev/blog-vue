a0c82cffdf199e4022c072b6e720807a897bb774
ZazaGdev <Gejadze250@gmail.com>
2023-09-13 12:05:24 +0400

    Introduced ref and reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+    <div class="home">
+        <p>{{ movieOne.name }} - {{ movieOne.rating }}</p>
+        <button @click="updateMovieOne">Movie one</button>
+        <p>{{ movieTwo.name }} - {{ movieTwo.rating }}</p>
+        <button @click="updateMovieTwo">Movie Two</button>
+    </div>
 </template>
 
 <script>
-
+import { reactive, ref } from 'vue'
 export default {
-  name: 'HomeView',
+    name: 'HomeView',
+    setup() {
+        const movieOne = ref({ name: 'titanic', rating: 4 })
+        const movieTwo = reactive({ name: 'avatar', rating: 6 })
+        const updateMovieOne = () => {
+            movieOne.value.rating++
+        }
+
+        const updateMovieTwo = () => {
+            movieTwo.rating++
+        }
+
+        return { movieOne, movieTwo, updateMovieTwo, updateMovieOne }
+    },
 }
 </script>
